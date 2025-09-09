b5a52c079b1212eb57f4bef222589fcb5b4c184e
ТЁТЯ_ЗИНА <_@DESKTOP-7L5CID9>
2025-09-09 20:36:14 +0300

    Исправил баг с счётчиком товаров в крозине. И внес небольшие изменения в интерфейс админа.

--- BookShoppingCartMvcUI/Repoditories/CartRepository.cs
+++ BookShoppingCartMvcUI/Repoditories/CartRepository.cs
@@ -122,13 +122,14 @@ namespace BookShoppingCartMvcUI.Repoditories
         }
         public async Task<int> GetCartItemCount(string userId ="")
         {
-            if (!string.IsNullOrEmpty(userId))
+            if (string.IsNullOrEmpty(userId))
             {
                 userId = GetUserId();
             }
             var data = await (from cart in _db.ShoppingCarts
                               join cartDetail in _db.CartDetails
                               on cart.Id equals cartDetail.ShoppingCartId
+                              where cart.UserId == userId
                               select new { cartDetail.Id }
                               ).ToListAsync();
             return data.Count;
